401acdd02a9df4b0573ccf1ba023d8b7fd104177
yuan.yinchun <yuanyinchun@yuanyinchuns-Mac.local>
2014-09-05 12:22:39 +0800

    Initial Commit

---
+++ RadialProgress/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RadialProgress
+//
+//  Created by yuan.yinchun on 5/9/14.
+//  Copyright (c) 2014 yyc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
